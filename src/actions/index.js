b0c20edf99f71163a5758b20255d6ffacc4cc7f2
Andy Chen <amazingandyyy@gmail.com>
2017-11-02 20:21:59 -0700

    ico prototype daap done

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,8 @@ export const connectEth = () => function (dispatch) {
   var eth
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
-    // eth = new Eth(window.web3.currentProvider);
-    eth = new Eth(new Eth.HttpProvider('http://localhost:8545'))
+    eth = new Eth(window.web3.currentProvider);
+    // eth = new Eth(new Eth.HttpProvider('http://localhost:8545'))
   } else {
     eth = new Eth(new Eth.HttpProvider('http://localhost:8545'))
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux'
 
 import root from './root'
-import Eth from './eth'
+import eth from './eth'
 
 export default combineReducers({
   root,
-  Eth
+  eth
 })
